6f85c5c03112775f24c79ef81463ca0e98b51bf2
wilsonify <tom.andrew.wilson@gmail.com>
2026-08-13 08:44:30 -0400

    feat: add PyScript support module and update package configuration

---
+++ pyknit/pyscript/__init__.py
@@ -0,0 +1,7 @@
+# pyKnit PyScript support modules
+# This package is included in the wheel for browser demos
+
+from pyknit.pyscript._assets import shared
+from pyknit.pyscript._demos import *
+
+__all__ = ["shared"]
